54d8df17f91d3fa6a98c4e1d4a53dc3ea01a5780
Jonathon Terry <jonathonterry93@gmail.com>
2020-12-07 23:15:38 -0700

    Add more conditionals and output to setup scriptZsh script

--- setupZsh.sh
+++ setupZsh.sh
@@ -27,22 +27,63 @@ else
   echo "ZSH is already installed."
 fi
 
-echo -n "Do you wish to make ZSH your default shell? (Yy/Nn)"
-read answer
-if [[ $answer != "${answer#[Yy]}" ]]; then
-  if [[ $SHELL != $(command -v zsh) ]]; then
+if [[ $SHELL != $(command -v zsh) ]]; then
+  echo -n "Do you wish to make ZSH your default shell? (Yy/Nn)"
+  read answer
+  if [[ $answer != "${answer#[Yy]}" ]]; then
     echo "Making ZSH your default shell."
 #    chsh -s $(command -v zsh)
   fi
-    echo "ZSH was already your default shell."
+else
+  echo "ZSH is already your default shell."
 fi
    
 echo ""
-echo -n "Do you wish to install Oh-My-Zsh?"
-read answer
-if [[ $answer != "${answer#[Yy]}" ]]; then
-  echo "Installing Oh-My-Zsh."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [[ -d "~/.oh-my-zsh" ]]; then
+  echo -n "Do you wish to install Oh-My-Zsh?"
+  read answer
+  if [[ $answer != "${answer#[Yy]}" ]]; then
+    echo "Installing Oh-My-Zsh."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  fi
+else
+  echo "Oh-My-ZSH is already installed"
+fi
+
+echo ""
+if [[ -d "~/.oh-my-zsh" ]]; then
+  echo -n "Would you like to install the 'zsh-syntax-highlighting' plugin? (Yy/Nn)"
+  read answer
+  if [[ $answer != "${answer#[Yy]}" ]]; then
+    echo "Cloning zsh-users/zsh-syntax-highlighting.git"
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+  fi
+else
+  echo "'zsh-syntax-highlighting' plugin is already installed"
+fi
+
+echo ""
+if [[ -d "~/.oh-my-zsh" ]]; then
+  echo -n "Would you like to install the 'history-search-multi-word' plugin? (Yy/Nn)"
+  read answer
+  if [[ $answer != "${answer#[Yy]}" ]]; then
+    echo "Cloning zdharma/history-search-multi-word.git"
+    git clone git://github.com/zdharma/history-search-multi-word.git $ZSH/custom/plugins/history-search-multi-word
+  fi
+else
+  echo "'history-search-multi-word' plugin is already installed"
+fi
+
+echo ""
+if [[ -d "~/.oh-my-zsh" ]]; then
+  echo -n "Would you like to install the 'zsh-autosuggestions' plugin? (Yy/Nn)"
+  read answer
+  if [[ $answer != "${answer#[Yy]}" ]]; then
+    echo "Cloning zsh-users/zsh-autosuggestions.git"
+    git clone git://github.com/zsh-users/zsh-autosuggestions.git $ZSH/custom/plugins/zsh-autosuggestions
+  fi
+else
+  echo "'zsh-autosuggestions' plugin is already installed"
 fi
 
 ## The below is broken - please fix!!!
@@ -52,26 +93,6 @@ fi
 #echo "Copying ZSH theme to ZSH Themes dir."
 #cp -a ./shellconfig/zed.zsh-theme $ZSH/custom/themes/
 
-echo -n "Would you like to install the 'zsh-syntax-highlighting' plugin? (Yy/Nn)"
-read answer
-if [[ $answer != "${answer#[Yy]}" ]]; then
-  echo "Cloning zsh-users/zsh-syntax-highlighting.git"
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
-fi
-
-echo -n "Would you like to install the 'history-search-multi-word' plugin? (Yy/Nn)"
-read answer
-if [[ $answer != "${answer#[Yy]}" ]]; then
-  echo "Cloning zdharma/history-search-multi-word.git"
-  git clone git://github.com/zdharma/history-search-multi-word.git $ZSH/custom/plugins/history-search-multi-word
-fi
-
-echo -n "Would you like to install the 'zsh-autosuggestions' plugin? (Yy/Nn)"
-read answer
-if [[ $answer != "${answer#[Yy]}" ]]; then
-  echo "Cloning zsh-users/zsh-autosuggestions.git"
-  git clone git://github.com/zsh-users/zsh-autosuggestions.git $ZSH/custom/plugins/zsh-autosuggestions
-fi
- 
+echo ""
 echo "Installation of ZSH and Oh-My-Zsh is complete. Please restart your terminals for changes to take effect."
-
+exit
