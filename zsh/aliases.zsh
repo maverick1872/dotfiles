b6a3446fa86dfff66fadeb8523879b4f12af3198
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-02-18 23:36:56 -0700

    update git log format

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias list-path-dirs='sed "s/:/\n/g" <<< "$PATH"'
 alias update-system='sudo apt update -y && sudo apt upgrade -y'
 
 # GIT
-alias glo='git log --pretty=format:"%Cgreen%cs - %Cred%h - %<(15,trunc)%Cgreen%an - %Creset%s" --date=local'
+alias glo='git log --pretty=format:"%Cred%cs%Creset - %C(auto)%h%Creset - %<(16,trunc)%Cgreen%an%Creset - %s %C(auto)%d" --date=local'
 alias gcuwm='gau && gcmsg'
 alias gbdlocal='git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d'
 #alias git-rm-local-merged='git branch -d $(git branch --merged | cut -c 3- | grep -i -v master | grep -i -v develop | grep -i -v integration | grep -v "*")'
