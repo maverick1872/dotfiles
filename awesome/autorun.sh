082545cce69a266272a6c496ee5052dc805d1d30
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-06-11 21:43:49 -0600

    update autorun

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-xrandr --output DP-0.8 --rotate right
-xrandr --output DP-0.1.1.8 --rotate left
+xrandr --output DP-0.8 --primary
+xrandr --output DP-0.1.8 --rotate left
+xrandr --output DP-0.1.1.8 --rotate right
