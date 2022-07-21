63af2755da604987adf21640343f3fcb16a2e9af
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-07-21 07:27:54 -0600

    explicit xrandr script

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-xrandr --output DP-0.8 --primary
-xrandr --output DP-0.1.8 --rotate left
-xrandr --output DP-0.1.1.8 --rotate right
+xrandr --output DisplayPort-0 --off \
+  --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate left \
+  --output DisplayPort-2 --off \
+  --output HDMI-A-0 --off \
+  --output DisplayPort-3 --mode 1920x1200 --pos 1200x360 --rotate normal \
+  --output DisplayPort-4 --off \
+  --output DisplayPort-5 --mode 1920x1200 --pos 3120x0 --rotate right \
+  --output DisplayPort-6 --off
