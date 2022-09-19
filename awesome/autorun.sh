bea5e8b4d8cc143303b9adeed058833dedb5b12d
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-09-18 22:17:25 -0600

    update awesome autorun

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Display configurations
-xrandr \
-  --output DisplayPort-0 --mode 3440x1440 --pos 1200x75 --rotate normal \
-  --output DisplayPort-1 --off \
-  --output DisplayPort-2 --mode 1920x1200 --pos 0x0 --rotate left  \
-  --output HDMI-A-0 --off \
-  --output DisplayPort-3 --mode 1920x1200 --pos 4640x0 --rotate right \
-  --output DisplayPort-4 --off
+xrandr --output DisplayPort-0 --mode 3440x1440 --pos 1200x75 --rotate normal
+xrandr --output DisplayPort-2 --mode 1920x1200 --pos 0x0 --rotate left
+xrandr --output DisplayPort-3 --mode 1920x1200 --pos 4640x0 --rotate right
+
+# Potential fix for output name issue
+#xrandr --newmode "2560x1440_33.00"  162.77  2560 2688 2960 3360  1440 1441 1444 1468  -HSync +Vsync
+#xrandr --addmode DisplayPort-0 2560x1440_33.00
+#xrandr --auto --output DisplayPort-0 --mode 2560x1440_33.00 --primary --left-of DisplayPort-1
 
 # Keyboard repeat rate for XOrg TODO: this should be moved to xinitrc??
 xset r rate 180 15
