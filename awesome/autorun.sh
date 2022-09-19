54fc506780be005e4cd588df91a08ce7c07c305b
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-09-18 22:24:12 -0600

    correct autorun

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -3,7 +3,7 @@
 # Display configurations
 xrandr --output DisplayPort-0 --mode 3440x1440 --pos 1200x75 --rotate normal
 xrandr --output DisplayPort-2 --mode 1920x1200 --pos 0x0 --rotate left
-xrandr --output DisplayPort-3 --mode 1920x1200 --pos 4640x0 --rotate right
+xrandr --output DisplayPort-4 --mode 1920x1200 --pos 4640x0 --rotate right
 
 # Potential fix for output name issue
 #xrandr --newmode "2560x1440_33.00"  162.77  2560 2688 2960 3360  1440 1441 1444 1468  -HSync +Vsync
