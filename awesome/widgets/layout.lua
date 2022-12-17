118cc03dc0b0861a367e4fc8900746a799f9ee02
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-12-16 23:45:47 -0700

    work on layout widgets

--- awesome/widgets/layout.lua
+++ awesome/widgets/layout.lua
@@ -1,9 +1,23 @@
 local awful = require("awful")
 local gears = require("gears")
+local wibox = require("wibox")
 
 -- Create an imagebox widget which will contain an icon indicating which layout we're using.
 return function(s)
+  local currentTag = awful.screen.focused().selected_tag
+  local column_count_widget = wibox.widget.textbox(currentTag.column_count)
+  local master_count_widget = wibox.widget.textbox(currentTag.master_count)
   local layoutWidget = awful.widget.layoutbox(s)
+
+  --for _, tag in ipairs(s.tags) do
+  --  tag.connect_signal("property::column_count", function(t)
+  --    column_count_widget.text = " " .. tostring(t.column_count)
+  --  end)
+  --  tag.connect_signal("property::master_count", function(t)
+  --    master_count_widget.text = " " .. tostring(t.master_count)
+  --  end)
+  --end
+
 
   layoutWidget:buttons(gears.table.join(
     awful.button({ }, 1, function()
@@ -20,5 +34,10 @@ return function(s)
     end)
   ))
 
-  return layoutWidget
-end
+  return {
+    layout = wibox.layout.fixed.horizontal,
+    layoutWidget,
+    --column_count_widget,
+    --master_count_widget,
+  }
+end;
