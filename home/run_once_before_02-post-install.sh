a1f826ce3f408aaead4fdaa7e7982ffafeac16f3
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-05-23 09:37:38 -0600

    chore: add log statement

--- home/run_once_before_02-post-install.sh
+++ home/run_once_before_02-post-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "${INTERACTIVE}" == "true" ]]; then
+  echo "Start login for Bitwarden"
   BW_SESSION=$(bw login --raw)
   bw sync
   echo export BW_SESSION=${BW_SESSION}
