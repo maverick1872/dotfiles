7ee77821f619235e109460d57eb6288ce6e60c21
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-06-01 12:31:58 -0600

    feat: ensure persisted token is cleaned up on manual lock/logout

--- home/dot_config/zsh/functions.zsh
+++ home/dot_config/zsh/functions.zsh
@@ -174,7 +174,6 @@ __load_nvm() {
 
 # Wrapper for BitWarden CLI to auto unlock vault and persist session when attempting to sync
 bw() {
-
   if [[ ! -d "${HOME}/.cache/bw" ]]; then
     mkdir -p ${HOME}/.cache/bw
   fi
@@ -183,33 +182,39 @@ bw() {
     export BW_SESSION=$(cat ${HOME}/.cache/bw/session)
   fi
 
-  if [[ $1 == "sync" ]]; then
-    BW_STATUS=$(command bw status | jq -r .status)
-    case "$BW_STATUS" in
-      "unauthenticated")
-        echo "Logging into BitWarden"
-        TOKEN=$(command bw login --raw)
-        if [[ $? -eq 0 ]]; then
-          export BW_SESSION=${TOKEN}
-          echo ${TOKEN} > ${HOME}/.cache/bw/session
-        fi
-        ;;
-      "locked")
-        echo "Unlocking Vault"
-        TOKEN=$(command bw unlock --raw)
-        if [[ $? -eq 0 ]]; then
-          export BW_SESSION=${TOKEN}
-          echo ${TOKEN} > ${HOME}/.cache/bw/session
-        fi
-        ;;
-      "unlocked")
-        ;;
-      *)
-        echo "Unknown Login Status: $BW_STATUS"
-        return 1
-        ;;
-    esac
-  fi
+  case "$1" in 
+    "sync")
+      BW_STATUS=$(command bw status | jq -r .status)
+      case "$BW_STATUS" in
+        "unauthenticated")
+          echo "Logging into BitWarden"
+          TOKEN=$(command bw login --raw)
+          if [[ $? -eq 0 ]]; then
+            export BW_SESSION=${TOKEN}
+            echo ${TOKEN} > ${HOME}/.cache/bw/session
+          fi
+          ;;
+        "locked")
+          echo "Unlocking Vault"
+          TOKEN=$(command bw unlock --raw)
+          if [[ $? -eq 0 ]]; then
+            export BW_SESSION=${TOKEN}
+            echo ${TOKEN} > ${HOME}/.cache/bw/session
+          fi
+          ;;
+        "unlocked")
+          ;;
+        *)
+          echo "Unknown Login Status: $BW_STATUS"
+          return 1
+          ;;
+      esac
+      ;;
+    "lock" | "logout")
+      rm ${HOME}/.cache/bw/session
+      ;;
+  esac
+  # fi
 
   command bw $@
 }
