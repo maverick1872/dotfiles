775c0387945b8dabdd27cbf1c40e4f72f1336e1a
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-06-07 09:54:08 -0600

    feat: improve media play script to pause all players prior to starting any other player

--- home/dot_config/awesome/utils/scripts/executable_enhancedPlayMedia.sh
+++ home/dot_config/awesome/utils/scripts/executable_enhancedPlayMedia.sh
@@ -1,10 +1,33 @@
-playerctl status
-playerStatus=$?
+#!/usr/bin/env zsh
 
-if [[ playerStatus -ne 0 ]]; then
+playerctl --all-players status
+playersFound=$?
+
+# If no player is available, start spotify
+if [[ playersFound -ne 0 ]]; then
   spotify &
   sleep 2
 fi
 
-playerctl --player=spotify,%any play-pause
-c
+playerStatus=($(playerctl --all-players status))
+playerNames=($(playerctl --all-players -f '{{playerName}}' status))
+targetPlayerIdx=$playerStatus[(Ie)Playing]
+
+# Determine least priority player to pause first
+if [[ $targetPlayerIdx > 0 ]]; then
+  overallStatus='Playing'
+  targetPlayer=${playerNames[$targetPlayerIdx]}
+else
+  overallStatus='Paused'
+fi
+
+# Pause all players prior to starting a player
+case $overallStatus in
+  'Paused')
+    playerctl --player=spotify,%any play
+    ;;
+ 'Playing')
+    playerctl --player=$targetPlayer pause
+    ;;
+esac
+
