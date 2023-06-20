1e07d34fe8754ea1a1e62ea885abd1409a1e4673
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-06-20 12:48:57 -0600

    feat: add fzf to pr checkout

--- home/dot_config/zsh/functions/github.zsh
+++ home/dot_config/zsh/functions/github.zsh
@@ -1,6 +1,19 @@
 # Checkout Github PR
 gcopr() {
-  gh pr checkout "$1"
+  if [[ -n $1 ]]; then
+    gh pr checkout "$1"
+  else
+    GH_FORCE_TTY=100% gh pr list \
+      | fzf --prompt='Filter PRs: ' \
+        --border \
+        --height '~50%' \
+        --reverse \
+        --ansi \
+        --header-lines 3 \
+        --preview 'GH_FORCE_TTY=100% gh pr view {1}' \
+        --preview-window 'down' \
+      | awk '{print $1}' | xargs gh pr checkout
+  fi
 }
 
 # View Github PR
