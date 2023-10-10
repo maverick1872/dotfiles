327b15d9c7d8cbd3112737ee65e4bc49ef33fa36
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-10 16:59:53 -0700

    feat: setup autocommands for help windows and editing chezmoi sources

--- home/dot_config/nvim-configs/custom/lua/autocmds.lua
+++ home/dot_config/nvim-configs/custom/lua/autocmds.lua
@@ -1,10 +1,28 @@
+local autoGrp = vim.api.nvim_create_augroup
+local autoCmd = vim.api.nvim_create_autocmd
+local expand = vim.fn.expand
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = autoGrp('YankHighlight', { clear = true })
+autoCmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+-- Moves the help window to the far right 
+autoCmd('FileType', {
+  pattern = 'help',
+  command = 'wincmd L',
+})
+
+-- Auto apply chezmoi source to target when written
+autoCmd('BufWritePost', {
+  pattern = expand '~' .. '/.local/share/chezmoi/*',
+  callback = function()
+     vim.cmd 'silent !chezmoi apply --source-path "<afile>:p"'
+  end
 })
