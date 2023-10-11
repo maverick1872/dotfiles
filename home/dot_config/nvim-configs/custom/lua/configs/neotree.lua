6588a3793cf80aea6c752b224c016a7b813fa72c
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-10 22:06:50 -0700

    feat: update neotree configuration to only hide gitignore files by default

--- home/dot_config/nvim-configs/custom/lua/configs/neotree.lua
+++ home/dot_config/nvim-configs/custom/lua/configs/neotree.lua
@@ -132,6 +132,10 @@ return {
     follow_current_file = { enabled = true },
     hijack_netrw_behavior = "open_current",
     use_libuv_file_watcher = true,
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    }
   },
   event_handlers = {
     {
