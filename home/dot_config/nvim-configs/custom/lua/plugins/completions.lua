6ec4ab7651687c772a2207ef6a776cffed6474bd
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-10 23:31:44 -0700

    feat: fixup completions configuration

--- home/dot_config/nvim-configs/custom/lua/plugins/completions.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/completions.lua
@@ -1,92 +1,113 @@
 return {
   'hrsh7th/nvim-cmp',
   event = 'VeryLazy',
-  -- config = function()
-  --   local cmp = require 'cmp'
-  --
-  --   local has_words_before = function()
-  --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  --     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-  --   end
-  --
-  --   local feedkey = function(key, mode)
-  --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-  --   end
-  --
-  --   cmp.setup {
-  --     snippet = {
-  --       expand = function(args)
-  --         vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
-  --       end,
-  --     },
-  --     mapping = {
-  --       ['<Left>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-  --       ['<Right>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-  --       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-  --       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-  --       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-  --       ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-  --       ['<C-e>'] = cmp.mapping {
-  --         i = cmp.mapping.abort(),
-  --         c = cmp.mapping.close(),
-  --       },
-  --       ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  --       ['<Tab>'] = cmp.mapping(function(fallback)
-  --         if vim.fn['vsnip#available'](1) == 1 then
-  --           feedkey('<Plug>(vsnip-expand-or-jump)', '')
-  --         elseif has_words_before() then
-  --           cmp.complete()
-  --         else
-  --           fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-  --         end
-  --       end, { 'i', 's' }),
-  --
-  --       ['<S-Tab>'] = cmp.mapping(function()
-  --         if vim.fn['vsnip#jumpable'](-1) == 1 then
-  --           feedkey('<Plug>(vsnip-jump-prev)', '')
-  --         end
-  --       end, { 'i', 's' }),
-  --     },
-  --     sources = cmp.config.sources({
-  --       { name = 'nvim_lsp' },
-  --       { name = 'vsnip' }, -- For vsnip users.
-  --     }, {
-  --       { name = 'buffer' },
-  --     }),
-  --   }
-  --
-  --   cmp.setup.filetype('gitcommit', {
-  --     sources = cmp.config.sources({
-  --       { name = 'cmp_git' },
-  --     }, {
-  --       { name = 'buffer' },
-  --     }),
-  --   })
-  --
-  --   cmp.setup.cmdline('/', {
-  --     sources = {
-  --       { name = 'buffer' },
-  --     },
-  --   })
-  --
-  --   cmp.setup.cmdline(':', {
-  --     mapping = cmp.mapping.preset.cmdline(),
-  --     sources = cmp.config.sources({
-  --       { name = 'path' },
-  --     }, {
-  --       { name = 'cmdline' },
-  --     }),
-  --   })
-  -- end,
+  config = function()
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    require("luasnip/loaders/from_vscode").lazy_load()
+    --   local has_words_before = function()
+    --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+    --   end
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = {
+        ['<CR>'] = cmp.mapping.confirm { select = false }, -- Accept explictly selected item.
+        ['<Space>'] = cmp.mapping.confirm { select = false }, -- Accept explictly selected item.
+        ['<Esc>'] = cmp.mapping.abort(), -- Close completions and restore line
+        ['<Left>'] = cmp.mapping.close(), -- Close completions and restore line
+        ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ['<Right>'] = cmp.mapping.confirm { select = false }, -- Accept explictly selected item.
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+            -- elseif check_backspace() then -- Source: lazynvim
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      },
+      formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+          -- vim_item.kind = string.format('%s', icons[vim_item.kind])
+          vim_item.menu = ({
+            luasnip = '[snip]',
+            buffer = '[nearby]',
+            path = '[path]',
+          })[entry.source.name]
+
+          return vim_item
+        end,
+      },
+      sources = {
+        -- {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        -- c
+        -- { name = 'buffer' },
+        { name = 'path' },
+        -- },
+      },
+      experimental = {
+        ghost_text = true,
+      },
+    }
+
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+        { name = 'cmp_git' },
+      }, {
+        { name = 'buffer' },
+      }),
+    })
+
+    cmp.setup.cmdline('/', {
+      sources = {
+        { name = 'buffer' },
+      },
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        { name = 'cmdline' },
+      }),
+    })
+  end,
+  -- possible additions would be:
+  -- cmp-npm
+  -- search nvim-cmp repo for aditional sources
   dependencies = {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-    -- 'L3MON4D3/LuaSnip',
-    -- 'rafamadriz/friendly-snippets',
-    -- 'hrsh7th/cmp-vsnip',
-    -- 'hrsh7th/vim-vsnip',
-    -- 'hrsh7th/vim-vsnip-integ',
+    'hrsh7th/cmp-git',
+    'L3MON4D3/LuaSnip',
+    'rafamadriz/friendly-snippets',
   },
 }
