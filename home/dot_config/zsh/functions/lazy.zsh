0a37de291ab8f2d45fddf548f7f0c40aa40897e6
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-10 18:04:45 -0700

    chore: cleanup node version autoload function. ensure fnm is installed via package managers

--- home/dot_config/zsh/functions/lazy.zsh
+++ home/dot_config/zsh/functions/lazy.zsh
@@ -1,36 +1,35 @@
 # Traverses PWD and up to find and autoload an nvmrc if exists. Compatible with lazy loading.
-__autoload-nvmrc() {
+__autoload-node-version() {
   local _path=$PWD
-  local nvmrc_path
+  local node_version_file
 
   if [[ "${_path}" != *"/dev/"* ]]; then
     return
   fi
 
   # Traverse parent directories until an nvmrc is encountered or root dev directory is encountered
-  while [[ "${_path}" != "" && "${_path}" == *"/home/\w+/dev/"* && ! -e "${_path}/.nvmrc" ]]; do
+  while [[ "${_path}" != "" && "${_path}" == *"/home/\w+/dev/"* && (! -e "${_path}/.nvmrc" || ! -e "${_path}/.node-version") ]]; do
     _path=${_path%/*}
   done
 
-  # Check if nvmrc exists in the final directory from above. Implementation replicates nvm_find_nvmrc
+  # Check if .nvmrc file exists in the final directory from above.
   if [ -e "${_path}/.nvmrc" ]; then
-    nvmrc_path="${_path}/.nvmrc"
+    node_version_file="${_path}/.nvmrc"
   fi
 
-  # Load appropriate NVM version
-  if [[ -n "${nvmrc_path}" ]]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+  # Check if .node-verision file exists in the final directory from above.
+  if [ -e "${_path}/.node-version" ]; then
+    node_version_file="${_path}/.node-version"
+  fi
 
-    if [[ "${nvmrc_node_version}" = "N/A" ]]; then
-      nvm install
-    elif [[ "${nvmrc_node_version}" != "$(nvm version)" ]]; then
-      nvm use
-    fi
-  elif [[ "$(nvm version)" != "$(nvm version default)" ]]; then
-    nvm use default
+  # Load appropriate NodeJS version
+  if [[ -n "${node_version_file}" ]]; then
+    fnm use --install-if-missing
+  elif [[ "$(fnm version)" != "$(fnm version default)" ]]; then
+    fnm use default
   fi
 }
 
 # autoload -U add-zsh-hook
-# add-zsh-hook chpwd __autoload-nvmrc
+# add-zsh-hook chpwd __autoload-node-version
 
