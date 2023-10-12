a7e4721f8a5ea0db3f072954cfedaaa6a160cb0f
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-11 21:11:54 -0700

    feat: ensure directory is set correctly when nvim is called w/ a specific dir

--- home/dot_config/nvim-configs/custom/init.lua
+++ home/dot_config/nvim-configs/custom/init.lua
@@ -31,5 +31,7 @@ require 'keymaps'
 -- Set colorscheme
 vim.cmd 'colorscheme astrodark'
 
+vim.cmd 'if argc() == 1 && isdirectory(argv(0)) | cd `=argv(0)` | endif'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
