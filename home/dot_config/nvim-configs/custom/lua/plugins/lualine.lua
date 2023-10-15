830e2cb11916bb4cfcc4720be1848958c8bf08e2
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-15 11:33:55 -0700

    feat: configure lualine and leverge bubbles theme

--- home/dot_config/nvim-configs/custom/lua/plugins/lualine.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/lualine.lua
@@ -1,13 +1,68 @@
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+local colors = {
+  blue = '#80a0ff',
+  cyan = '#79dac8',
+  black = '#080808',
+  white = '#c6c6c6',
+  red = '#ff5189',
+  violet = '#d183e8',
+  grey = '#303030',
+  -- astrodark = '#1b1f27',
+  -- astrodark = nil,
+  astrodark = '#121317',
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.black, bg = colors.astrodark },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.astrodark },
+    b = { fg = colors.white, bg = colors.astrodark },
+    c = { fg = colors.black, bg = colors.astrodark },
+  },
+}
+
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  enabled = true,
   opts = {
     options = {
-      icons_enabled = false,
-      theme = 'onedark',
+      theme = bubbles_theme,
       component_separators = '|',
-      section_separators = '',
+      section_separators = { left = '', right = '' },
     },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 2 },
+      },
+      lualine_b = { 'filename', 'branch', 'diff' },
+      lualine_c = { --[[ 'fileformat' ]]
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
   },
 }
