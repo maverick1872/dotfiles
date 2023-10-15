1fe2279e52d429128cbd9519e5763c01288a209b
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-14 22:07:43 -0700

    fix: correct null-ls autoformat handler

--- home/dot_config/nvim-configs/custom/lua/plugins/null-ls.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/null-ls.lua
@@ -13,14 +13,15 @@ return {
         if client.supports_method 'textDocument/formatting' then
           vim.api.nvim_clear_autocmds {
             group = lspFormatGroup,
-            buffer = vim.api.nvim_create_autocmd('BufWritePre', {
-              group = lspFormatGroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format()
-              end,
-            }),
+            buffer = bufnr,
           }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = lspFormatGroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
         end
       end,
     }
