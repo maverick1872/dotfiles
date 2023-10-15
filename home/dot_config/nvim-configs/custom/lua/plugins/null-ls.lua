9feda80e0e53bd9cdce80468ce1d62f13e94926f
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-14 19:26:48 -0700

    feat: configure autoformatting on BufPreWrite for null-ls

--- home/dot_config/nvim-configs/custom/lua/plugins/null-ls.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/null-ls.lua
@@ -1,11 +1,28 @@
+local lspFormatGroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'jose-elias-alvarez/null-ls.nvim',
+  -- event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "williamboman/mason.nvim",
-    "jay-babu/mason-null-ls.nvim",
+    'williamboman/mason.nvim',
+    'jay-babu/mason-null-ls.nvim',
   },
-  config = function ()
-    require('null-ls').setup()
+  config = function()
+    require('null-ls').setup {
+      on_attach = function(client, bufnr)
+        if client.supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds {
+            group = lspFormatGroup,
+            buffer = vim.api.nvim_create_autocmd('BufWritePre', {
+              group = lspFormatGroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format()
+              end,
+            }),
+          }
+        end
+      end,
+    }
   end,
 }
