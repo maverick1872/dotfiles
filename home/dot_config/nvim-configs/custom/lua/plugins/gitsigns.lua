f92ff82f3c1db21ab0430dc3182a1a1d8d7dc5df
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-23 09:53:58 -0700

    chore: format gitsigns plugin configuration

--- home/dot_config/nvim-configs/custom/lua/plugins/gitsigns.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/gitsigns.lua
@@ -17,15 +17,23 @@ return {
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
       vim.keymap.set({ 'n', 'v' }, ']c', function()
-        if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
       vim.keymap.set({ 'n', 'v' }, '[c', function()
-        if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
     end,
   },
 }
