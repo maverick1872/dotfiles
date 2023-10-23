4202d8bbb66cce73c848206cd99284ce2b950db2
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-23 14:13:01 -0700

    feat: add clock to statusline

--- home/dot_config/nvim-configs/custom/lua/plugins/lualine.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/lualine.lua
@@ -44,15 +44,22 @@ return {
     },
     sections = {
       lualine_a = {
-        { 'mode', separator = { left = '' }, right_padding = 2 },
+        {
+          'mode',
+          separator = { left = '' },
+          right_padding = 2,
+        },
       },
       lualine_b = { 'filename', 'branch', 'diff' },
-      lualine_c = { --[[ 'fileformat' ]]
-      },
+      lualine_c = {},
       lualine_x = {},
-      lualine_y = { 'filetype', 'progress' },
+      lualine_y = { 'filetype', 'location' },
       lualine_z = {
-        { 'location', separator = { right = '' }, left_padding = 2 },
+        {
+          'os.date("%I:%M", os.time())',
+          separator = { right = '' },
+          left_padding = 2,
+        },
       },
     },
     inactive_sections = {
