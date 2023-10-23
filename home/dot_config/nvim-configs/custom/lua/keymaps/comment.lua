9a86144731b2092fbd5a84329e0400d92c352186
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-23 09:52:59 -0700

    fix: correct comment keymaps to function correctly in visual mode

--- home/dot_config/nvim-configs/custom/lua/keymaps/comment.lua
+++ home/dot_config/nvim-configs/custom/lua/keymaps/comment.lua
@@ -1,27 +1,12 @@
 local is_available = require('utils').is_available
 local map = require('utils').map
+local comment = require 'Comment.api'
 
--- Comment
 if is_available 'Comment.nvim' then
-  map('n', '<leader>/', function()
-    require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-  end, 'Toggle comment line')
-  map('v', '<leader>/', function()
-    vim.api.nvim_feedkeys('esc', 'nx', false)
-    require('Comment.api').toggle.blockwise(vim.fn.visualmode())
+  map('n', '<leader>/', comment.toggle.linewise.current, 'Toggle comment for current line')
+  map('x', '<leader>/', function()
+    local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'x', false)
+    comment.toggle.linewise(vim.fn.visualmode())
   end, 'Toggle comment for selection')
-
-
-
-        -- -- Toggle selection (linewise)
-        -- vim.keymap.set('x', '<leader>c', function()
-        --     vim.api.nvim_feedkeys(esc, 'nx', false)
-        --     api.toggle.linewise(vim.fn.visualmode())
-        -- end)
-        --
-        -- -- Toggle selection (blockwise)
-        -- vim.keymap.set('x', '<leader>b', function()
-        --     vim.api.nvim_feedkeys(esc, 'nx', false)
-        --     api.toggle.blockwise(vim.fn.visualmode())
-        -- end)
 end
