f701042a071970482385e3f6dd21f39b3ae578ac
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-10-25 08:02:02 -0700

    feat: add current line blame virt text and update gutter icons

--- home/dot_config/nvim-configs/custom/lua/plugins/gitsigns.lua
+++ home/dot_config/nvim-configs/custom/lua/plugins/gitsigns.lua
@@ -5,14 +5,23 @@ return {
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
+      add = { text = '│' },
+      change = { text = '│' },
       delete = { text = '_' },
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
+      untracked = { text = '┆' },
     },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 800,
+      ignore_whitespace = false,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
     on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+      vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
