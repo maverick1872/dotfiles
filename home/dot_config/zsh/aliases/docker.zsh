851362435f52bfc2ce900b71f015e4f9985d2c98
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-11-03 15:33:57 -0700

    chore: remove no aliases and functions that have migrated to plugin impls

--- home/dot_config/zsh/aliases/docker.zsh
+++
@@ -1,4 +0,0 @@
-alias clean-images='docker images -aq | xargs docker rmi'
-alias clean-containers='docker ps -aq | xargs docker rm'
-alias clean-volumes='docker volume ls -q | xargs docker volume rm'
-
--- home/dot_config/zsh/aliases/node.zsh
+++
@@ -1,5 +0,0 @@
-# NODE & NPM
-alias update-npm='npm install -g npm'
-alias npmid='npm i -D'
-alias npmip='npm i -P'
-
