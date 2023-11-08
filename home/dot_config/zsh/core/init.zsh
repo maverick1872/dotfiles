b85a3d5f06be88aea7a0f04703ed2a49d67d67d2
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2023-11-08 11:30:24 -0800

    chore: refine plugin loading to enable easier disabling

--- home/dot_config/zsh/core/init.zsh
+++ home/dot_config/zsh/core/init.zsh
@@ -4,6 +4,22 @@ source $ZDOTDIR/core/env.zsh
 # Load all options
 source $ZDOTDIR/core/options.zsh
 
+# Define Plugins to load
+plugins=(
+  bitwarden
+  chezmoi
+  core
+  docker
+  fnm
+  gcloud
+  github
+  node
+  rust
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  history-search-multi-word
+)
+
 # Load OMZ if not disabled
 if [[ -z $NO_OMZ ]]; then
   # Autosuggest -- requires in order: zsh-syntax-highlighting zsh-autosuggestions
@@ -13,36 +29,23 @@ if [[ -z $NO_OMZ ]]; then
   # Which plugins would you like to load
   # Standard plugins can be found in ${ZSH}/plugins/*
   # Custom plugins may be added to ${ZSH_CUSTOM}/plugins/*
-  plugins=(
-    bitwarden
-    chezmoi
-    core
-    docker
-    fnm
-    gcloud
-    git
-    github
-    node
-    rust
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    history-search-multi-word
-  )
+  plugins+=(git)
 
   source $ZSH/oh-my-zsh.sh
   # Add alias to load zsh without OMZ
   alias custom-zsh="NO_OMZ=1 zsh"
 else
-  ## Consider moving the following order dependent setup to a a standalone file e.g. init.zsh or similar
+  plugins+=(git-custom)
+  
   # Load Completion Configuration
   source $ZDOTDIR/core/completion.zsh
 
   # Add all themes to fpath and load the chosen theme if applicable
-  FPATH="${ZDOTDIR}/omz/themes/:${FPATH}"
+  FPATH="${ZDOTDIR}/themes/:${FPATH}"
   autoload -Uz ${ZTHEME:=robbyrussell}.zsh-theme; ${ZTHEME:=robbyrussell}.zsh-theme
 
   # Load all plugins
-  for file in $(find ${ZDOTDIR}/plugins -type f -name '*.plugin.zsh' -print); do
-    source "$file"
+  for plugin ($plugins); do
+    source "${ZDOTDIR}/plugins/$plugin/$plugin.plugin.zsh"
   done
 fi
