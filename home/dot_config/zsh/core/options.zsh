d5b3b60b53ff6ff84aa74a6c7b0696f8d90cec5e
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-01-01 11:08:17 -0800

    chore: document omz provided options

--- home/dot_config/zsh/core/options.zsh
+++ home/dot_config/zsh/core/options.zsh
@@ -1,1 +1,25 @@
-setopt HIST_SAVE_NO_DUPS       		# Do not write a duplicate event to the history file.
+setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
+setopt PROMPT_SUBST    	      # Enable parameter expansion, command substitution and arithmetic expansion in the prompt.
+
+
+# Options enabled when using oh-my-zsh
+# alwaystoend           on
+# autocd                on
+# autopushd             on
+# completeinword        on
+# extendedhistory       on
+# noflowcontrol         on
+# histexpiredupsfirst   on
+# histignoredups        on
+# histignorespace       on
+# histsavenodups        on
+# histverify            on
+# interactive           on
+# interactivecomments   on
+# longlistjobs          on
+# promptsubst           on
+# pushdignoredups       on
+# pushdminus            on
+# sharehistory          on
+# shinstdin             on
+# transientrprompt      on
