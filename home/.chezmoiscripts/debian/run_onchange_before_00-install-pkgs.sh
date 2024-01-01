9dca18d6585f5567e691e9ee6ae05bc729058142
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-01-01 10:01:06 -0800

    chore: update debian package script

--- home/.chezmoiscripts/debian/run_onchange_before_00-install-pkgs.sh
+++ home/.chezmoiscripts/debian/run_onchange_before_00-install-pkgs.sh
@@ -2,6 +2,9 @@
 
 sudo apt update && sudo apt install <<EOF
 ripgrep
-tealdeer
-neovim
+fzf
 EOF
+
+# The following are installed via other means but are listed here for reference
+# neovim
+# tealdeer
