549816cbdfccfce08aec4bc76af086e19b0e1eb0
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-01-08 15:41:01 -0800

    fix(zsh): detect brew install when it's not available via commands

--- home/dot_config/zsh/plugins/brew/brew.plugin.zsh
+++ home/dot_config/zsh/plugins/brew/brew.plugin.zsh
@@ -1,9 +1,28 @@
 if (( ! $+commands[brew] )); then
-  _debug 'Brew is not installed'
-  return
-fi
+  if [[ -n "$BREW_LOCATION" ]]; then
+    if [[ ! -x "$BREW_LOCATION" ]]; then
+      echo "[oh-my-zsh] $BREW_LOCATION is not executable"
+      return
+    fi
+  elif [[ -x /opt/homebrew/bin/brew ]]; then
+    BREW_LOCATION="/opt/homebrew/bin/brew"
+  elif [[ -x /usr/local/bin/brew ]]; then
+    BREW_LOCATION="/usr/local/bin/brew"
+  elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    BREW_LOCATION="/home/linuxbrew/.linuxbrew/bin/brew"
+  elif [[ -x "$HOME/.linuxbrew/bin/brew" ]]; then
+    BREW_LOCATION="$HOME/.linuxbrew/bin/brew"
+  else
+    _debug 'Brew is not installed'
+    return
+  fi
 
-eval "$(brew shellenv)"
+  # Only add Homebrew installation to PATH, MANPATH, and INFOPATH if brew is
+  # not already on the path, to prevent duplicate entries. This aligns with
+  # the behavior of the brew installer.sh post-install steps.
+  eval "$("$BREW_LOCATION" shellenv)"
+  unset BREW_LOCATION
+fi
 
 if [[ -d "$HOMEBREW_PREFIX/share/zsh/site-functions" ]]; then
   fpath+=("$HOMEBREW_PREFIX/share/zsh/site-functions")
