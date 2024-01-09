318d8b0d59d1c2c4acfe6db9706c4fc837257318
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-01-08 18:07:45 -0800

    wip(zsh): decouple prompt from omz

---
+++ home/dot_config/zsh/themes/utils.zsh
@@ -0,0 +1,130 @@
+build_prompt () {
+	local promptString="" 
+	if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
+	then
+		promptString+="${yellow}[SSH@%m]${reset} " 
+	fi
+	git_root=$(git rev-parse --show-toplevel 2> /dev/null) 
+	if [[ ! -n $git_root ]]
+	then
+		promptString+="$current_dir_output%1(j. $jobs_bg.)" 
+		echo $promptString
+		return
+	fi
+	repo=$(git config --get remote.origin.url | sed -r 's/^.*\:(.*)\.git/\1/') 
+	repo_parts=(${(s|/|)repo}) 
+	repo_org="$magentab$repo_parts[1]$cyanb@$reset" 
+	repo_name="$magentab$repo_parts[2]$reset" 
+	if [[ $git_root = $PWD ]]
+	then
+		promptString+="$repo_org$repo_name%1(j. $jobs_bg.)" 
+		echo $promptString
+		return
+	fi
+	internal_repo_path=$(echo $PWD | sed 's|'"$git_root"'/||') 
+	repo_dir="$blueb$internal_repo_path$reset" 
+	promptString+="$repo_org$repo_name/$repo_dir%1(j. $jobs_bg.)" 
+	echo $promptString
+}
+
+git_status_info () {
+	local trimmed_branch truncated_branch
+	if [[ -n $(git_prompt_info) ]]
+	then
+		trimmed_branch=$(git_prompt_info | sed -r 's/.*((sc-|jira-).*).*/\1/') 
+		info="%25>…>$trimmed_branch%>>" 
+		info+=$(git_prompt_status)$(git_remote_status) 
+		printf %s " $whiteb($reset$yellow$info$whiteb)$reset $blue| "
+	fi
+}
+
+git_remote_status () {
+	local remote ahead behind git_remote_status git_remote_status_detailed
+	remote=${$(__git_prompt_git rev-parse --verify ${hook_com[branch]}@{upstream} --symbolic-full-name 2>/dev/null)/refs\/remotes\/} 
+	if [[ -n ${remote} ]]
+	then
+		ahead=$(__git_prompt_git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l) 
+		behind=$(__git_prompt_git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l) 
+		if [[ $ahead -eq 0 ]] && [[ $behind -eq 0 ]]
+		then
+			git_remote_status="$ZSH_THEME_GIT_PROMPT_EQUAL_REMOTE" 
+		elif [[ $ahead -gt 0 ]] && [[ $behind -eq 0 ]]
+		then
+			git_remote_status="$ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE" 
+			git_remote_status_detailed="$ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE_COLOR$ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE$((ahead))%{$reset_color%}" 
+		elif [[ $behind -gt 0 ]] && [[ $ahead -eq 0 ]]
+		then
+			git_remote_status="$ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE" 
+			git_remote_status_detailed="$ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE_COLOR$ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE$((behind))%{$reset_color%}" 
+		elif [[ $ahead -gt 0 ]] && [[ $behind -gt 0 ]]
+		then
+			git_remote_status="$ZSH_THEME_GIT_PROMPT_DIVERGED_REMOTE" 
+			git_remote_status_detailed="$ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE_COLOR$ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE$((ahead))%{$reset_color%}$ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE_COLOR$ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE$((behind))%{$reset_color%}" 
+		fi
+		if [[ -n $ZSH_THEME_GIT_PROMPT_REMOTE_STATUS_DETAILED ]]
+		then
+			git_remote_status="$ZSH_THEME_GIT_PROMPT_REMOTE_STATUS_PREFIX${remote:gs/%/%%}$git_remote_status_detailed$ZSH_THEME_GIT_PROMPT_REMOTE_STATUS_SUFFIX" 
+		fi
+		echo $git_remote_status
+	fi
+}
+
+git_prompt_status () {
+	[[ "$(__git_prompt_git config --get oh-my-zsh.hide-status 2>/dev/null)" = 1 ]] && return
+	local -A prefix_constant_map
+	prefix_constant_map=('\?\? ' 'UNTRACKED' 'A  ' 'ADDED' 'M  ' 'ADDED' 'MM ' 'MODIFIED' ' M ' 'MODIFIED' 'AM ' 'MODIFIED' ' T ' 'MODIFIED' 'R  ' 'RENAMED' ' D ' 'DELETED' 'D  ' 'DELETED' 'UU ' 'UNMERGED' 'ahead' 'AHEAD' 'behind' 'BEHIND' 'diverged' 'DIVERGED' 'stashed' 'STASHED') 
+	local -A constant_prompt_map
+	constant_prompt_map=('UNTRACKED' "$ZSH_THEME_GIT_PROMPT_UNTRACKED" 'ADDED' "$ZSH_THEME_GIT_PROMPT_ADDED" 'MODIFIED' "$ZSH_THEME_GIT_PROMPT_MODIFIED" 'RENAMED' "$ZSH_THEME_GIT_PROMPT_RENAMED" 'DELETED' "$ZSH_THEME_GIT_PROMPT_DELETED" 'UNMERGED' "$ZSH_THEME_GIT_PROMPT_UNMERGED" 'AHEAD' "$ZSH_THEME_GIT_PROMPT_AHEAD" 'BEHIND' "$ZSH_THEME_GIT_PROMPT_BEHIND" 'DIVERGED' "$ZSH_THEME_GIT_PROMPT_DIVERGED" 'STASHED' "$ZSH_THEME_GIT_PROMPT_STASHED") 
+	local status_constants
+	status_constants=(UNTRACKED ADDED MODIFIED RENAMED DELETED STASHED UNMERGED AHEAD BEHIND DIVERGED) 
+	local status_text
+	status_text="$(__git_prompt_git status --porcelain -b 2> /dev/null)" 
+	if [[ $? -eq 128 ]]
+	then
+		return 1
+	fi
+	local -A statuses_seen
+	if __git_prompt_git rev-parse --verify refs/stash &> /dev/null
+	then
+		statuses_seen[STASHED]=1 
+	fi
+	local status_lines
+	status_lines=("${(@f)${status_text}}") 
+	if [[ "$status_lines[1]" =~ "^## [^ ]+ \[(.*)\]" ]]
+	then
+		local branch_statuses
+		branch_statuses=("${(@s/,/)match}") 
+		for branch_status in $branch_statuses
+		do
+			if [[ ! $branch_status =~ "(behind|diverged|ahead) ([0-9]+)?" ]]
+			then
+				continue
+			fi
+			local last_parsed_status=$prefix_constant_map[$match[1]] 
+			statuses_seen[$last_parsed_status]=$match[2] 
+		done
+	fi
+	for status_prefix in ${(k)prefix_constant_map}
+	do
+		local status_constant="${prefix_constant_map[$status_prefix]}" 
+		local status_regex=$'(^|\n)'"$status_prefix" 
+		if [[ "$status_text" =~ $status_regex ]]
+		then
+			statuses_seen[$status_constant]=1 
+		fi
+	done
+	local status_prompt
+	for status_constant in $status_constants
+	do
+		if (( ${+statuses_seen[$status_constant]} ))
+		then
+			local next_display=$constant_prompt_map[$status_constant] 
+			status_prompt="$next_display$status_prompt" 
+		fi
+	done
+	echo $status_prompt
+}
+
+__git_prompt_git () {
+	GIT_OPTIONAL_LOCKS=0 command git "$@"
+}
