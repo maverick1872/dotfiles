05e14d41fd7c14c81bfb621ea6a23ce11276037d
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-01-09 21:42:14 -0800

    chore(zsh): document options for standardization

--- home/dot_config/zsh/core/options.zsh
+++ home/dot_config/zsh/core/options.zsh
@@ -1,6 +1,6 @@
 setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
 setopt PROMPT_SUBST    	      # Enable parameter expansion, command substitution and arithmetic expansion in the prompt.
-
+setopt PUSHD_IGNORE_DUPS      # Do not store duplicates in the directory stack.
 
 # Options enabled when using oh-my-zsh
 # alwaystoend           on
@@ -23,3 +23,16 @@ setopt PROMPT_SUBST    	      # Enable parameter expansion, command substitution
 # sharehistory          on
 # shinstdin             on
 # transientrprompt      on
+
+
+
+# Options enabled when using custom zsh config
+# completeinword
+# histsavenodups
+# interactive
+# menucomplete
+# monitor
+# promptsubst
+# pushdignoredups
+# shinstdin
+# transientrprompt
