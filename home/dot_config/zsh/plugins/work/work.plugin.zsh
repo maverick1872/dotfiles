412b1adcfb242fbc4a590a918554e243a6412a1b
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2024-04-08 08:38:20 -0700

    fix: ensure cloudflare function awaits service start and general cleanup

--- home/dot_config/zsh/plugins/work/work.plugin.zsh
+++ home/dot_config/zsh/plugins/work/work.plugin.zsh
@@ -1,15 +1,20 @@
 cloudflare() {
   strict_mode
-  service_running=$(systemctl is-active --quiet warp-svc.service)
+  service_running=$(systemctl is-active warp-svc.service)
   
-  if [[ $service_running == "NO" ]]; then
+  if [[ $service_running != "active" ]]; then
     sudo systemctl start warp-svc.service
+		sleep 1
   fi
 
   if [[ $1 == "on" ]]; then
+		echo "Connecting to Cloudflare..."
     command warp-cli connect
   elif [[ $1 == "off" ]]; then
+		echo "Disconnecting from Cloudflare..."
     command warp-cli disconnect
+  elif [[ $1 == "status" ]]; then
+    command warp-cli status
   fi
 
   strict_mode off
