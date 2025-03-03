0951c577fea891736c28db8ac09ce5d3b91c45d7
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2025-03-02 22:44:28 -0800

    fix: disable and fix broken import on heirline until this is pursued

--- home/dot_config/nvim/lua/plugins/heirline.lua
+++ home/dot_config/nvim/lua/plugins/heirline.lua
@@ -1,9 +1,10 @@
 return {
   'rebelot/heirline.nvim',
+  enabled = false,
   cond = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  opts = require('configs.heirline').Opts,
+  -- opts = require('configs.heirline').Opts,
   -- config = require 'configs.heirline',
 }
