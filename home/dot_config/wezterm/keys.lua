562ff2a711d62adbfaea35bdd3a04910742698bb
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2025-03-06 21:30:11 -0800

    chore: add couple keybinds and define mod first for easier reading

--- home/dot_config/wezterm/keys.lua
+++ home/dot_config/wezterm/keys.lua
@@ -1,19 +1,24 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 
 local module = {}
 
 function module.apply_to_config(config)
 	-- TODO: this needs to be updated to insert rather than override the table?
 	config.keys = {
-		{ key = "X", mods = "CTRL", action = wezterm.action.ActivateCopyMode },
+
+		{ mods = "CTRL", key = "X", action = action.ActivateCopyMode },
 		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+		{ mods = "OPT", key = "LeftArrow", action = action({ SendString = "\x1bb" }) },
 		-- Make Option-Right equivalent to Alt-f; forward-word
-		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+		{ mods = "OPT", key = "RightArrow", action = action({ SendString = "\x1bf" }) },
 		-- Make Cmd-Left/Right equivalent to Home/End
-		{ key = "LeftArrow", mods = "CMD", action = wezterm.action({ SendString = "\x1bOH" }) },
-		{ key = "RightArrow", mods = "CMD", action = wezterm.action({ SendString = "\x1bOF" }) },
-		{ key = "4", mods = "LEADER", action = wezterm.action({ SpawnCommandInNewTab = { cwd = wezterm.home_dir } }) },
+		{ mods = "CMD", key = "LeftArrow", action = action({ SendString = "\x1bOH" }) },
+		{ mods = "CMD", key = "RightArrow", action = action({ SendString = "\x1bOF" }) },
+		{ mods = "CMD", key = "R", action = action.ReloadConfiguration },
+		{ mods = "CMD", key = "K", action = action.ClearScrollback("ScrollbackAndViewport") },
+
+		{ mods = "LEADER", key = "4", action = action({ SpawnCommandInNewTab = { cwd = wezterm.home_dir } }) },
 	}
 end
 
