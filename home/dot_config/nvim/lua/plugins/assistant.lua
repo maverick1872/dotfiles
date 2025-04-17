9c27c87c7306a57c8c642f366c7b063d31798135
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2025-04-16 17:36:48 -0700

    feat(nvim): update avante configuration to use copilot for a provider and ensure auto suggestions are explicitly disabled

--- home/dot_config/nvim/lua/plugins/assistant.lua
+++ home/dot_config/nvim/lua/plugins/assistant.lua
@@ -3,25 +3,24 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
-    -- for example
-    -- provider = "openai",
-    -- openai = {
-    --   endpoint = "https://api.openai.com/v1",
-    --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    --   temperature = 0,
-    --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-    -- },
+    provider = "copilot",
+    copilot = {
+      model = "claude-3.7-sonnet",
+      temperature = 0,
+      max_tokens = 8192,
+    },
+    auto_suggestions_provider = "claude",   -- This should not be Copilot until https://github.com/yetone/avante.nvim/issues/1048 is addressed
+    behaviour = {
+      auto_suggestions = false
+    }
   },
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+
     --- The below dependencies are optional,
     "echasnovski/mini.pick",         -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
