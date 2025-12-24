0999d0b93c4a00ba625e2ff90b81482235c76939
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2025-12-23 17:36:00 -0800

    fix(nvim): try and bring parity to zsh rg and neovime rg

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -4,4 +4,22 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+
+        -- Match your usual behavior:
+        '--hidden',
+        '--ignore-file',
+        os.getenv('RIPGREP_CONFIG_DIR') .. '/ignore',
+      },
+    },
+  },
 }
